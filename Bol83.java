69df7d0bd7b939274081807c32b24876349a8860
Iago00 <56074077+Iago00@users.noreply.github.com>
2019-11-19 08:58:09 +0100

    bol8-3

---
+++ Bol83.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol8.pkg3;
+
+/**
+ *
+ * @author ipampillonvieitez
+ */
+public class Bol83 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Meses objeto = new Meses();
+        objeto.datos();
+        System.out.println(objeto.diasMes());
+    
+    }
+    
+}
